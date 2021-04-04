8617a899f2babd9ecfb660071a9e30a9af504c14
HM-cm <80047510+HM-cm@users.noreply.github.com>
2021-04-04 22:31:56 +0300

    Add files via upload

---
+++ BasePlayer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace JobApplicationProj3
+{
+    abstract class BasePlayer
+    {
+        protected Canvas canvas;
+        public Image image;
+        public Point point;
+        protected ImageSource[] arrUp;
+        protected ImageSource[] arrDown;
+        public ImageSource[] arrLeft;
+        protected ImageSource[] arrRight;
+        public DispatcherTimer animationTimer;
+        public DispatcherTimer moveTimer;
+        protected int dx;
+        protected int dy;
+        public int animationCount = 0;
+        public Direction direction { get; set; }
+
+        public BasePlayer(Canvas myCanvas)
+        {
+            this.canvas = myCanvas;
+            this.image = new Image();
+            this.image.Height = 40;
+            this.image.Width = 40;
+            this.point = new Point(490,435);
+            Canvas.SetLeft(this.image, this.point.X);
+            Canvas.SetTop(this.image, this.point.Y);
+            this.canvas.Children.Add(this.image);
+            this.arrUp = new ImageSource[3];
+            this.arrDown = new ImageSource[3];
+            this.arrLeft = new ImageSource[3];
+            this.arrRight = new ImageSource[3];
+            this.animationTimer = new DispatcherTimer();
+            this.animationTimer.Interval = TimeSpan.FromTicks(1);
+            this.animationTimer.Tick += AnimationTimer_Tick;
+            this.moveTimer = new DispatcherTimer();
+            this.moveTimer.Interval = TimeSpan.FromTicks(1);
+            this.moveTimer.Tick += MoveTimer_Tick;
+            this.dx = 3;
+            this.dy = 3;
+        }
+
+        public virtual void MoveTimer_Tick(object sender, object e)
+        {
+            
+        }
+
+        public virtual void AnimationTimer_Tick(object sender, object e)
+        {
+            
+        }
+    }
+}
---
+++ Player3.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace JobApplicationProj3
+{
+    class Player3:BasePlayer
+    {
+        public Player3(Canvas myCanvas) : base(myCanvas)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                this.arrUp[i] = new BitmapImage(new Uri("ms-appx:///Assets/C3/CUp/CUp" + (i + 1) + ".png"));
+                this.arrDown[i] = new BitmapImage(new Uri("ms-appx:///Assets/C3/CDown/CDown" + (i + 1) + ".png"));
+                this.arrLeft[i] = new BitmapImage(new Uri("ms-appx:///Assets/C3/CLeft/CLeft" + (i + 1) + ".png"));
+                this.arrRight[i] = new BitmapImage(new Uri("ms-appx:///Assets/C3/CRight/CRight" + (i + 1) + ".png"));
+            }
+            this.image.Source = arrLeft[0];
+            this.animationCount++;
+        }
+        public override void AnimationTimer_Tick(object sender, object e)
+        {
+            if (this.animationCount > 2)
+            {
+                this.animationCount = 0;
+            }
+            switch (this.direction)
+            {
+                case Direction.Left:
+                    this.image.Source = this.arrLeft[animationCount];
+                    this.animationCount++;
+                    break;
+                case Direction.Right:
+                    this.image.Source = this.arrRight[animationCount];
+                    this.animationCount++;
+                    break;
+                case Direction.Up:
+                    this.image.Source = this.arrUp[animationCount];
+                    this.animationCount++;
+                    break;
+                case Direction.Down:
+                    this.image.Source = this.arrDown[animationCount];
+                    this.animationCount++;
+                    break;
+            }
+        }
+        public override void MoveTimer_Tick(object sender, object e)
+        {
+            if (this.animationTimer.IsEnabled == false)
+            {
+                this.animationTimer.Start();
+            }
+            switch (this.direction)
+            {
+                case Direction.Left:
+                    this.point.X -= this.dx;
+                    break;
+                case Direction.Right:
+                    this.point.X += this.dx;
+                    break;
+                case Direction.Up:
+                    this.point.Y -= this.dy;
+                    break;
+                case Direction.Down:
+                    this.point.Y += this.dy;
+                    break;
+            }
+            if ((this.point.X <= 0 && this.direction == Direction.Left) || (this.point.X >= 480 && this.direction == Direction.Right))
+            {
+                this.dx = 0;
+                this.animationTimer.Stop();
+            }
+            else
+            {
+                this.dx = 5;
+            }
+            if ((this.point.Y <= -10 && this.direction == Direction.Up) || (this.point.Y >= 500 && this.direction == Direction.Down))
+            {
+                this.dy = 0;
+                this.animationTimer.Stop();
+            }
+            else
+            {
+                this.dy = 5;
+            }
+            Canvas.SetLeft(this.image, this.point.X);
+            Canvas.SetTop(this.image, this.point.Y);
+        }
+    }
+}
